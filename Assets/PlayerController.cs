fc477c7a9795e7f2409a23d773740e286b54bea1
bsauberman <bsauberman@gmail.com>
2020-09-08 18:39:46 -0400

    First commit baby

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveMult; //Multiplier for movement
+    public float turnMult; //Multiplier for turning
+    public float jumpMult; //Jump force multiplier
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        rb = GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Input.GetAxis("Horizontal") * moveMult /10f, 0, Input.GetAxis("Vertical") * moveMult/10f);
+        transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * turnMult);
+        Debug.Log(Input.GetAxis("Mouse X"));
+
+        if(Input.GetButton("Jump")) {
+            rb.AddForce(Vector2.up * jumpMult);
+        }
+    }
+}
