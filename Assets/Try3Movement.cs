dba753505828e0c6add61ffa4ad08a9ecab24c71
Nick <nggreen22@gmail.com>
2020-09-10 13:58:13 -0400

    First Choice Working
    A is up, S is right

--- Assets/Try3Movement.cs
+++ Assets/Try3Movement.cs
@@ -10,8 +10,10 @@ public class Try3Movement : MonoBehaviour
 
        GameObject[] tileArray = new GameObject[57];
        [SerializeField] public int currPos = 0;
-       Color white = new Color(1,1,1,1);
 
+       [SerializeField] public int choiceTile = 0;
+
+//       Color white = new Color(1,1,1,1);
 
 
     // Start is called before the first frame update
@@ -19,6 +21,7 @@ public class Try3Movement : MonoBehaviour
     {
             int x = 0;
             tileArray = GameObject.FindGameObjectsWithTag("Tile"); 
+            int count = 0;
 
        
         for(int i = currPos; i<= currPos + (diceRoll - 1); i++ ){
@@ -27,24 +30,35 @@ public class Try3Movement : MonoBehaviour
                 transform.position = tileArray[i].transform.position;
                 x = i;
 
-                if(tileArray[i].GetComponent<Renderer>().material.color == white) {
+                if(i == 6) {
                     print("Which way would you like to go?");
-                    int y = 0;
-                    if (y == 0) {
+                    print("A = Up    S = Right");
+                    yield return new WaitForSeconds(3);
+                    if (choiceTile == 0) {
                     }
-                    if (y ==1) {
-                        x = x;
-                    }
+                    else if (choiceTile == 1) {
+                        diceRoll = diceRoll - count;
+                        i = 30;
+                        currPos = 30;
+                    } 
+                    
                 }
+                count++; 
 
         }
         currPos = x + 1;
     }
+
      
 
     // Update is called once per frame
     void Update()
     {
+        if ((Input.GetKeyDown (KeyCode.A))) {
+            choiceTile = 0;
+        } else if ((Input.GetKeyDown (KeyCode.S))) {
+            choiceTile = 1;
+        }
 
     }
 
