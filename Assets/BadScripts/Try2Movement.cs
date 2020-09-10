e30ade9e5dc4c69df37de710e5ac3665a185c01a
Nick <nggreen22@gmail.com>
2020-09-09 21:03:22 -0400

    Player Movement on Board
    put in Dice Roll, he will move that many spaces (1 through 6). Options not implemented yet

---
+++ Assets/BadScripts/Try2Movement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Try2Movement : MonoBehaviour
+{
+
+    [SerializeField] Vector3 movementVector = new Vector3(-10f, 0f, 0f);
+    [SerializeField] float period = 4f;
+    
+
+    //todo remove from inspector
+    [Range(0,1)] [SerializeField] float movementFactor;   // 0 for not moved, 1 for fully moved
+
+    Vector3 startingPos;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (period <= Mathf.Epsilon) {
+            return;
+        }
+        if (movementFactor >= 0.9) {
+            return;
+        }
+        float cycles = Time.time / period;  // grows continually from 0
+
+        const float tau = Mathf.PI * 2;  // about 6.28, just to get the tau value
+        float rawSinWave = Mathf.Sin(cycles * tau);
+
+
+        movementFactor = rawSinWave / 2f + 0.5f;
+       // Vector3 offset = movementVector * movementFactor;
+       Vector3 offset =  GameObject.Find("TileOne").transform.position;;
+      //  transform.position = startingPos + offset;
+       transform.position = offset;
+    }
+}
---
+++ Assets/Try3Movement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Try3Movement : MonoBehaviour
+{
+
+    public GameObject tile;
+    public GameObject nextTile;
+    public GameObject tileOption;
+    public int diceRoll;
+
+       GameObject[] tileArray = new GameObject[6];
+
+
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+            tileArray = GameObject.FindGameObjectsWithTag("Tile"); 
+
+
+        for(int i = 0; i<= diceRoll - 1; i++ ){
+
+                yield return new WaitForSeconds(1);
+                transform.position = tileArray[i].transform.position;
+                
+        }
+    }
+     
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
+    
+
