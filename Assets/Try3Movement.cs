454356472712c555c8d7b26ce4325d34781a3f60
Nick <nggreen22@gmail.com>
2020-09-09 22:13:03 -0400

    Board Complete, Player Position rememberred
    Not all tiles tagged, only first 7 work

--- Assets/Try3Movement.cs
+++ Assets/Try3Movement.cs
@@ -6,27 +6,39 @@ using UnityEngine;
 public class Try3Movement : MonoBehaviour
 {
 
-    public GameObject tile;
-    public GameObject nextTile;
-    public GameObject tileOption;
     public int diceRoll;
 
-       GameObject[] tileArray = new GameObject[6];
+       GameObject[] tileArray = new GameObject[57];
+       [SerializeField] public int currPos = 0;
+       Color white = new Color(1,1,1,1);
 
 
 
     // Start is called before the first frame update
     IEnumerator Start()
     {
+            int x = 0;
             tileArray = GameObject.FindGameObjectsWithTag("Tile"); 
 
-
-        for(int i = 0; i<= diceRoll - 1; i++ ){
+       
+        for(int i = currPos; i<= currPos + (diceRoll - 1); i++ ){
 
                 yield return new WaitForSeconds(1);
                 transform.position = tileArray[i].transform.position;
-                
+                x = i;
+
+                if(tileArray[i].GetComponent<Renderer>().material.color == white) {
+                    print("Which way would you like to go?");
+                    int y = 0;
+                    if (y == 0) {
+                    }
+                    if (y ==1) {
+                        x = x;
+                    }
+                }
+
         }
+        currPos = x + 1;
     }
      
 
