d5d7964446b6e564fd7b77c3c3a4555261e5019d
Nick <nggreen22@gmail.com>
2020-09-16 14:54:46 -0400

    Cleaned up board movement code

--- Assets/Try3Movement.cs
+++ Assets/Try3Movement.cs
@@ -8,7 +8,6 @@ public class Try3Movement : MonoBehaviour
 
     public int diceRoll;
     public bool round = true;
-    GameObject[] tileArray = new GameObject[57];
         
 
     // Player Information
@@ -31,9 +30,6 @@ public class Try3Movement : MonoBehaviour
     {
         currTile = GameObject.Find("StartingBlock");
       
-
-            int x = 0;
-            tileArray = GameObject.FindGameObjectsWithTag("Tile"); 
 while (round) {            
             int i = currPos;
             bool turn = true;
@@ -61,93 +57,12 @@ while (turn) {
             currTile = currTile.GetComponent<tileHolder>().nextTile;
             }
             currPos++;
-/*
-                yield return new WaitForSeconds(1);
-                transform.position = tileArray[i].transform.position;
-                x = i;
 
-                if(i == 6) {
-                    if (j == diceRoll - 1) {
-                        // do nothing
-                    } else { 
-                        print("Which way would you like to go?");
-                    print("A = Up    S = Right");
-                    yield return waitForKeyPress(KeyCode.L);
-                    if (choiceTile == 0) {
-                    }
-                    else if (choiceTile == 1) {
-                        i = 30;
-                    } 
-                    }
-                }
-                else if (i == 10) {
-                    if (j == diceRoll - 1) {
-                        // do nothing
-                    } else { 
-                    print("Which way would you like to go?");
-                    print("A = Up    S = Right");
-                    yield return waitForKeyPress(KeyCode.L);
-                    if (choiceTile == 0) {
-                    }
-                    else if (choiceTile == 1) {
-                        i = 38;
-                    } 
-                    }
-                }
-                else if(i == 14) {
-                    if (j == diceRoll - 1) {
-                        // do nothing
-                    } else { 
-                    print("Which way would you like to go?");
-                    print("A = Right    S = Down");
-                    yield return waitForKeyPress(KeyCode.L);
-                    if (choiceTile == 0) {
-                    }
-                    else if (choiceTile == 1) {
-                        i = 45;
-                    } 
-                    }
-                }
-                else if(i == 44) {
-                    if (j == diceRoll - 1) {
-                        // do nothing
-                    } else { 
-                    print("Which way would you like to go?");
-                    print("A = Right    S = Down");
-                    yield return waitForKeyPress(KeyCode.L);
-                    if (choiceTile == 0) {
-                        i = 52;
-                    }
-                    else if (choiceTile == 1) {
-                        i = 44;
-                    } 
-                    }
-                }
-
-                else if (i == 38) {
-                    i = 27;
-                }
-                else if (i == 30) {
-                    i = -1;
-                }
-                else if (i == 45) {
-                    i = 36;
-                }
-                else if (i == 52) {
-                    i = 22;
-                }
-                else if (i == 54) {
-                    i = 22;
-                }
-                i++;
-                */
         }
-        // might need to take off plus one to fix future rolls
-      //  currPos = x + 1;
         turn = false;
     }
 
-    checkTileAction(x);
+    checkTileAction(currTile);
     round = false;
     GameObject.Find("GameMaster").GetComponent<gameMaster>().y ++;
    
@@ -155,14 +70,12 @@ while (turn) {
 
 }
 
-private void checkTileAction(int currPos) {
+private void checkTileAction(GameObject currTile) {
 
-if (tileArray[currPos].GetComponent<Renderer>().material.color == blue) {
+    if (currTile.tag == "Tile") {
         coins = coins + 3;
     }
-else if (tileArray[currPos].GetComponent<Renderer>().material.color == white) {
-        coins = coins - 3;
-    }
+
 
 }
 
@@ -181,9 +94,6 @@ else if (tileArray[currPos].GetComponent<Renderer>().material.color == white) {
  
 }
  
-    
-
-     
 
     // Update is called once per frame
     void Update()
