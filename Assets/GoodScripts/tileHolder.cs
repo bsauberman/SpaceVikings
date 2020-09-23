1548acdaab708bbf8521878c584b8d05978dcb37
Nick <nggreen22@gmail.com>
2020-09-23 01:24:37 -0400

    HELLLL YESSSSS
    movement works

--- Assets/GoodScripts/tileHolder.cs
+++ Assets/GoodScripts/tileHolder.cs
@@ -10,10 +10,28 @@ public class tileHolder : MonoBehaviour
 
     [SerializeField] public int checker;
 
+    
+
     // Start is called before the first frame update
     void Start()
     {
+    GameObject player1 = GameObject.Find("player1");    
+    GameObject player2 = GameObject.Find("player2");
+    GameObject player3 = GameObject.Find("player3");
+    GameObject player4 = GameObject.Find("player4");
        // GameObject.DontDestroyOnLoad(this.gameObject);
+       if (transform.position == player1.transform.position) {
+           player1.GetComponent<Try3Movement>().currTile = this.gameObject;
+       }
+       if (transform.position == player2.transform.position) {
+           player2.GetComponent<Try3Movement>().currTile = this.gameObject;
+       }
+       if (transform.position == player3.transform.position) {
+           player3.GetComponent<Try3Movement>().currTile = this.gameObject;
+       }
+       if (transform.position == player4.transform.position) {
+           player4.GetComponent<Try3Movement>().currTile = this.gameObject;
+       }
         
     }
 
