84473fda5ee5340b9fddb0f024a2b5fd7ad9b6d5
Nick <nggreen22@gmail.com>
2020-09-24 22:39:58 -0400

    Minigame 2 prototype
    Asteroid Drop

---
+++ Assets/GoodScripts/meteorDestroy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class meteorDestroy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float lifetime = 10.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(lifetime > 0) {
+            lifetime -= Time.deltaTime;
+            if (lifetime <= 0) {
+                Destruction();
+            }
+        }
+        if (this.transform.position.y <= -20) {
+            Destruction();
+        }
+    
+    }
+
+    void Destruction() {
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/asteroidDrop.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class asteroidDrop : MonoBehaviour
+{
+
+    public GameObject spawnPos;
+    public GameObject spawnee;
+    int number = 0;
+    string spawnChoice;
+
+    int i;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        number = Random.Range(0,8);
+        spawnChoice = "Spawner" + number;
+        spawnPos = GameObject.Find(spawnChoice);
+        if (Time.time>i){               
+            Instantiate(spawnee,spawnPos.transform.position, spawnPos.transform.rotation);
+            i+=1;
+        }
+    }
+}
