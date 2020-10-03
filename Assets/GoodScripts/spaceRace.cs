ca91482c703b2ca0521298e3438c9274809a3ba0
Nick <nggreen22@gmail.com>
2020-10-02 21:45:20 -0400

    started space race 3 mechanics

---
+++ Assets/GoodScripts/spaceRace.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spaceRace : MonoBehaviour
+{
+    private float thrust = 10f;
+    public float rotationSpeed = 100.0f;
+    public int playerNum;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward * thrust);
+        float rotation = Input.GetAxis("P"+playerNum+"Y") * rotationSpeed;
+        rotation *= Time.deltaTime;
+        transform.Rotate(rotation,0,0);
+        
+    }
+}
