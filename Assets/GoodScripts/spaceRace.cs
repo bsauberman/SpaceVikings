1859f6e70cbca78a6354019b858aedb181ca6534
Nick <nggreen22@gmail.com>
2020-10-04 20:38:34 -0400

    Space Race massive improvement
    got all movement working

--- Assets/GoodScripts/spaceRace.cs
+++ Assets/GoodScripts/spaceRace.cs
@@ -4,23 +4,30 @@ using UnityEngine;
 
 public class spaceRace : MonoBehaviour
 {
-    private float thrust = 10f;
+    private float thrust = 150f;
     public float rotationSpeed = 100.0f;
     public int playerNum;
+
+    public int maxSpeed = 700000;
 
     // Start is called before the first frame update
     void Start()
     {
+        Rigidbody rigidbody = this.GetComponent<Rigidbody>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward * thrust);
+        GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward * thrust * Time.deltaTime);
         float rotation = Input.GetAxis("P"+playerNum+"Y") * rotationSpeed;
         rotation *= Time.deltaTime;
         transform.Rotate(rotation,0,0);
+
+        // if (GetComponent<Rigidbody>().velocity.magnitude > maxSpeed) {
+        //     GetComponent<Rigidbody>().velocity = GetComponent<Rigidbody>().velocity.normalized * maxSpeed;
+        // }
         
     }
 }
