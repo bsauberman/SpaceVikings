7ed077286451a21d6c92c629f64ebb2c961dafe1
Nick <nggreen22@gmail.com>
2020-10-08 17:38:05 -0400

    Third Person Movement Script way better

---
+++ Assets/GoodScripts/game3Move.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    private CharacterController controller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = this.GetComponent<CharacterController>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 moveVector = Vector3.zero;
+        moveVector.x = -(Input.GetAxis("Horizontal")) * 3;
+        moveVector.z = -(Input.GetAxis("Vertical")) * 3;
+
+        controller.Move(moveVector * Time.deltaTime);
+        
+    }
+}
